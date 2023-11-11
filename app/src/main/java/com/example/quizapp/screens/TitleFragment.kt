a11e0a832e49b58dae0c7cd616f8c1da521782db
Ayaat <93150311+Ayaats@users.noreply.github.com>
2023-11-11 02:30:40 +0200

    Navigation is done !!

--- app/src/main/java/com/example/quizapp/screens/TitleFragment.kt
+++ app/src/main/java/com/example/quizapp/screens/TitleFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.quizapp.R
+import com.example.quizapp.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
 
+    lateinit var binding: FragmentTitleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
 
+        return  binding.root
+}
 }
