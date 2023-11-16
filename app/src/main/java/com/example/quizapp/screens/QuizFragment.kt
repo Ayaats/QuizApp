fb63126e95c6fba0bdbb41ff4412b8b8eb82348d
Ayaat <93150311+Ayaats@users.noreply.github.com>
2023-11-16 02:03:57 +0200

    title screen is done

--- app/src/main/java/com/example/quizapp/screens/QuizFragment.kt
+++ app/src/main/java/com/example/quizapp/screens/QuizFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.quizapp.R
 import com.example.quizapp.databinding.FragmentQuizBinding
 
@@ -16,7 +17,8 @@ lateinit var binding: FragmentQuizBinding
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_quiz, container, false)
+        return binding.root
     }
 }
 
--- app/src/main/java/com/example/quizapp/screens/TitleFragment.kt
+++ app/src/main/java/com/example/quizapp/screens/TitleFragment.kt
@@ -5,27 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.quizapp.R
 import com.example.quizapp.databinding.FragmentTitleBinding
 
 
 class TitleFragment : Fragment() {
 
-    lateinit var binding: FragmentTitleBinding
+     lateinit var binding: FragmentTitleBinding
 
-    override fun onCreateView(
+     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+         binding.startBtn.setOnClickListener { view:View->
 
+             // checking if the name edittext is not empty
+             if (binding.etName.text!!.isNotEmpty()){
+                 /// takes us to the next fragment to play game
+                 view.findNavController().navigate(R.id.action_titleFragment_to_quizFragment)
+//                 val action = TitleFragmentDirections.actionTitleFragmentToQuizFragment()
+//                 action.name = binding.etName.text.toString()
+//                 findNavController().navigate(action)
+             } else{
+
+                 Toast.makeText(context, "Enter your name to start", Toast.LENGTH_SHORT).show()
+             }
+
+
+         }
         return  binding.root
 }
 }
